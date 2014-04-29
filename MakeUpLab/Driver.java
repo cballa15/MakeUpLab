c09842cb6e2fe235cc64efd482c9ec0b509931ba
C <tweakydragon@gmail.com>
2014-04-29 12:14:56 -0400

    Almost

---
+++ MakeUpLab/Driver.java
@@ -0,0 +1,15 @@
+/**
+* A very short driver for the GUI planets application
+* @author L. Lehmann
+* @version 4/27/2014
+*/
+
+public class Driver
+{
+	public static void main(String[] args)
+	{
+
+		//create an instance of the GUI class
+		ExampleGUIOne planetWeights = new ExampleGUIOne();
+	}
+}
